a72e6ba820cf770c8fa3dd61cbee212290e63485
StudentName <student@example.com>
2025-11-21 23:19:00 +0800

    完善内容

--- src/test/java/com/docker/mall/shopdatamanager/ShopDataManagerApplicationTests.java
+++ src/test/java/com/docker/mall/shopdatamanager/ShopDataManagerApplicationTests.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ShopDataManagerApplicationTests {
+public class ShopDataManagerApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
